9086d67a992e8e62bb8ba15899e3ae5f060a2ad8
NeoAtlasz <y0shii103288@gmail.com>
2025-06-17 22:32:46 +0000

    redjh

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 document.getElementById("greeting").textContent = "Hi there!";
 document.getElementById("h1").textContent = "The Header";
 
-let items = document.getElementsByClassName("redClass");
+let items = document.getElementsByClassName("redclass");
 for (let i = 0; i < items.length; i++)
 {
     items[i].textContent = "Text change";
 }
+
+const header = document.querySelector("h1");
+console.log(header);
+const red = document.querySelectorAll("redclass");
+console.log(red);
+document.getElementById("click").addEventListener("click",function()
+{
+    alert("CLICK BUTTON!");
+});
 /* "The header" */
 
 /*
