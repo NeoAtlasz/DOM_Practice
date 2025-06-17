f4e5c80a98e34a3b75c4d0795bdcb3ca9fade038
NeoAtlasz <y0shii103288@gmail.com>
2025-06-17 20:40:52 +0000

    grr

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("greeting").textContent = "Hi there!";
+document.getElementById("h1").textContent = "the header";
+/* "The header" */
+
+/*
+run this first: php -S localhost:3006 -t ./
+*/
