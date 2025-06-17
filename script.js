212c8d74c82955dccdabd4cdb4312d4907598ca9
NeoAtlasz <y0shii103288@gmail.com>
2025-06-17 21:11:08 +0000

    textchangpart2

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("greeting").textContent = "Hi there!";
 document.getElementById("h1").textContent = "The Header";
 
 let items = document.getElementsByClassName("redClass");
-for (let i = 0;i < items.length; i++)
+for (let i = 0; i < items.length; i++)
 {
     items[i].textContent = "Text change";
 }
